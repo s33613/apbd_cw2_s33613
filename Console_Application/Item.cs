3a78f63a3b7c9e26efbda10304e3b409c3d16c60
s33613 <267266068+s33613@users.noreply.github.com>
2026-03-22 11:26:42 +0100

    Addition of Return and Rent methods allowing to track items avialability

--- Console_Application/Item.cs
+++ Console_Application/Item.cs
@@ -1,10 +1,46 @@
-﻿using System.Xml.Schema;
+﻿using System;
+using System.Xml.Schema;
 
 namespace Console_Application
 {
     public class Item
     {
-        private string Name { get; set;}
-        private string Description { get; set;}
+        private string name { get; set;}
+        private string description { get; set;}
+        public bool isAvailable { get; private set; }
+
+        public Item(string nameP, string descriptionP)
+        {
+            name = nameP;
+            description = descriptionP;
+            isAvailable = true;
+        }
+
+        public void Return()
+        {
+            if (!isAvailable)
+            {
+                Console.WriteLine("Item " + name + " has been Returned");
+                isAvailable = true;
+            }
+            else
+            {
+                Console.WriteLine("Item " + name + " is already in Store");
+            }
+        }
+
+        public void Rent()
+        {
+            if (isAvailable)
+            {
+                Console.WriteLine("Item " + name + " has been Rented");
+                isAvailable = false;
+            }
+            else
+            {
+                Console.WriteLine("Item " + name + " is already in Rented");
+            }
+
+        }
     }
 }
