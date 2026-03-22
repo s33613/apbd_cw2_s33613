1304c567555e36c82b59d9731c483dc5cf5db9cd
s33613 <267266068+s33613@users.noreply.github.com>
2026-03-22 13:47:08 +0100

    Added classes Employee and Student

---
+++ Console_Application/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Console_Application
+{
+    public class Employee : User
+    {
+        private int rentLimit;
+
+        public Employee(string name, string password) : base(name, password)
+        {
+            this.rentLimit = 5;
+        }
+
+    }
+}
---
+++ Console_Application/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Console_Application
+{
+    public class Student : User
+    {
+        private int rentLimit;
+        Student(string name, string password) : base(name, password)
+        {
+            this.rentLimit = 2;
+        }
+    }
+}
