e893b2a65707795fea041299126bf609d02ce46f
s33613 <267266068+s33613@users.noreply.github.com>
2026-03-22 11:43:47 +0100

    Added User Class

---
+++ Console_Application/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Console_Application
+{
+    public class User
+    {
+        public string name {get; set;}
+        private string password { get; set;}
+
+        public User(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
