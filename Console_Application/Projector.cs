273ac8dcb578aa26460631fc10a2fc702c0d84bc
s33613 <267266068+s33613@users.noreply.github.com>
2026-03-22 13:17:26 +0100

    Added Projector Class

---
+++ Console_Application/Projector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Console_Application
+{
+    public class Projector : Item
+    {
+        public Projector(string desc) : base("Projector",desc)
+        {
+        }
+
+        public static void Show()
+        {
+            Console.WriteLine("Projecting an image");
+        }
+    }
+}
