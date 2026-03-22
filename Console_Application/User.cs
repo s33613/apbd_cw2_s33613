930790bb306f4a855b371541c03def134c9a3d4e
s33613 <267266068+s33613@users.noreply.github.com>
2026-03-22 11:55:53 +0100

    Added Login method

--- Console_Application/User.cs
+++ Console_Application/User.cs
@@ -1,14 +1,36 @@
-﻿namespace Console_Application
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_Application
 {
     public class User
     {
-        public string name {get; set;}
+        private static List<User> userList = new List<User>();
+        private string name {get; set;}
         private string password { get; set;}
 
         public User(string name, string password)
         {
             this.name = name;
             this.password = password;
+            userList.Add(this);
+        }
+
+        public void Login(string username,string password)
+        {
+            User u = userList.Find(user => user.name == username);
+            if (u != null)
+            {
+                if(u.password == password) {
+                    Console.WriteLine("successfully logged in");
+                }
+                else {
+                    Console.WriteLine("Invalid password");
+                }
+            }
+            else{
+                Console.WriteLine("Cannot find user with this username");
+            }
         }
     }
 }
